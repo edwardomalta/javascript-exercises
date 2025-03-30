1c595cf9be1de3f8ca9ba4d4761109008b79f17e
Edwardo Malta <edwardoalta@gmail.com>
2025-03-30 13:48:17 -0600

    Practice number 6 finished

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // First test: if it is divisible by 4
+    if (year % 4 === 0) {
+        // this could be a leap year. Next test:
+        if (year % 100 === 0) {
+            // this could be not a Leap year; 
+            if (year % 400 === 0 ) return true;
+            else return false;
+        } else return true;
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
